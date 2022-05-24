9000bc4cb8a790798ddf01c9621d48eb881dbe0f
Chris Middleton <ctjmiddleton@gmail.com>
2022-05-24 16:03:42 +0100

    Form test written - failing

--- contact_list/src/ContactModal/ContactModal.test.js
+++ contact_list/src/ContactModal/ContactModal.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import { ContactModal } from './';
+
+afterEach(cleanup)
 
 test('renders contact modal text', async () => {
   render(<ContactModal />);
   const text = await screen.findByText('I am the contact modal');
   expect(text).toBeInTheDocument();
 });
+
+test('initializes empty form', () => {
+    render(<ContactModal />);
+   const nameInput =  screen.queryAllByPlaceholderText('Name')
+   const phoneInput =  screen.queryAllByPlaceholderText('Phone Number')
+   const emailInput = screen.queryAllByPlaceholderText('EmailAddress')
+
+   expect(nameInput).toBeInTheDocument();
+   expect(phoneInput).toBeInTheDocument();
+   expect(emailInput).toBeInTheDocument();
+
+   expect(nameInput).toHaveValue("");
+   expect(phoneInput).toHaveValue("");
+   expect(emailInput).toHaveValue("");
+
+  });
