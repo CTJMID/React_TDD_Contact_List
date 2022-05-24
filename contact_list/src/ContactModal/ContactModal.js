6da488c3b4fcfc2affd64c804d4e231368889b6f
Chris Middleton <ctjmiddleton@gmail.com>
2022-05-24 16:33:09 +0100

    utilising use State to form

--- contact_list/src/ContactModal/ContactModal.js
+++ contact_list/src/ContactModal/ContactModal.js
@@ -1,19 +1,37 @@
- import styles from './styles.module.css'
+ 
+ import { useState } from 'react';
+ import styles from './styles.module.css';
  
  export const ContactModal = () => {
      
-    const
-    
-    
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
     
     return (
      <> 
      <div>I am the contact modal</div>
+     
      <div className={styles.main}>
          <form>
-        <input placeholder= 'Name'/>
-        <input placeholder= 'Phone Number' />
-        <input placeholder= 'Email Address' />
+        <input 
+            required
+            value={name}
+            placeholder= 'Name'
+            onChange={e => setName(e.target.value)}
+        />
+        <input 
+            required
+            value={phone}
+            placeholder= 'Phone Number'
+            onChange={e => setPhone(e.target.value)}
+        />
+        <input 
+            required
+            value={email}
+            placeholder= 'Email Address'
+            onChange={e => setEmail(e.target.value)
+        } />
 
         <buttom> Submit </buttom>
         </form>
